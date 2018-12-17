c0d2dec451de23353a35f6ddea2dbf657f6f2366
Adam Brown <acourtneybrown@github.com>
2018-12-17 14:11:03 -0700

    Fix to work for years 1995 to 2014

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ I used Python 3.6 with anaconda(updated). I have also updated the files in the o
 In this repository you can find scripts for obtaining [Fortune 1000](http://fortune.com/fortune500/) and [Global 500](http://fortune.com/global500/) companies lists and their data outputs. Scripts downloading and parsing information from the official site.
 
 ## Installation
- You should have Python 2.7 installed. In folder with cloned repository please run:
+ You should have Python 3.6 installed. In folder with cloned repository please run:
  
  ```terminal
- -sudo pip install virtualenv
- 
- virtualenv env
- source env/bin/activate
+ python3 -m venv .venv
+
+ . .venv/bin/activate
  
  pip install -r requirements.txt
  ```
@@ -23,7 +22,7 @@ In this repository you can find scripts for obtaining [Fortune 1000](http://fort
  ## Usage
  Don't forget to activate virtualenv if it isn't already done. To do that run:
  ```terminal
- source env/bin/activate
+ . .venv/bin/activate
  ```
  
  To obtain Fortune 1000 companies list please run the following. This will generate csv file in output folder.
--- fortune1000.py
+++ fortune1000.py
@@ -1,15 +1,18 @@
-# -*- coding: utf-8 -*-
-# Fortune 1000 grabber. Download and parse companies information
+#!/usr/bin/env python3
 
-# Import libraries
-# Original code used python 2.7. I did it using python 3.6. Also, this code works for the current website. 
-# Also. I added a little more meta-data.
-# last checked : 8th Spetember, 2017
+from bs4 import BeautifulSoup
 
-from json import JSONDecodeError
-import urllib.request
-import json
 import csv
+import json
+import logging
+import re
+import sys
+import urllib.request
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class Company:
     ranking = 0
@@ -33,8 +36,42 @@ class Company:
     hqstate = ""
     hqzip = ""
 
-def grab():
+
+def usage():
+    print("Usage: %s <year>" % sys.argv[0])
+    sys.exit(1)
+
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+
+    # Obtain companies
+    year = sys.argv[1]
+    companies = grab(year)
+    f = open(f"output/fortune1000-{year}.csv", "wt")
+
+    try:
+        writer = csv.writer(f)
+        writer.writerow(["ranking", "full name", "ticker", "industry", "sector", "hq location", "hq address", "years on list", "ceo", "eps",
+                         "employees", "revenues", "profits", "hq zip", "website", "market value", "global 500 rank", "revenue change", "profit change", "hq state"])
+
+        for company in companies:
+            writer.writerow([company.ranking, company.fullname, company.ticker, company.industry,
+                             company.sector, company.hqlocation, company.hqaddr, company.yearsonlist, company.ceo, company.eps,
+                             company.employees, company.revenues, company.profits, company.hqzip, company.website, company.mktval,
+                             company.global500rank, company.revchange, company.prftchange, company.hqstate])
+
+    finally:
+        f.close()
+
+
+def grab(year):
     # Obtaining post id
+    data = urllib.request.urlopen(f"http://fortune.com/fortune500/{year}/")
+    soup = BeautifulSoup(data,"html.parser")
+    postid = next(attr for attr in soup.body['class'] if attr.startswith('postid'))
+    postid = re.match(r'postid-(\d+)', postid).group(1)
     companies = []
 
     # Fetch for pages with data and process JSONs
@@ -68,31 +105,17 @@ def grab():
                     company.prftchange = item["meta"]["prftchange"]
                     company.hqstate = item["meta"]["hqstate"]
                 except KeyError:
-                    print("Keyerror has occurered for " + str(company.fullname))
+                    logger.error("Keyerror has occurered for " + str(company.fullname))
 
-                print(str(company.ranking) + ". " + str(company.fullname) + " ; " + str(company.ceo))
+                logger.info(str(company.ranking) + ". " + str(company.fullname) + " ; " + str(company.ceo))
 
                 companies.append(company)
-        except JSONDecodeError:
-            print(str(i) + " to " + str(i+100) + " has JSONDecodeError.")
+        except json.JSONDecodeError:
+            logger.error(str(i) + " to " + str(i+100) + " has JSONDecodeError.")
 
     return companies
 
 
-# Obtain companies
-companies = grab()
-f = open("output/fortune1000.csv", "wt")
+if __name__ == "__main__":
+    main()
 
-try:
-    writer = csv.writer(f)
-    writer.writerow(["ranking", "full name", "ticker", "industry", "sector", "hq location", "hq address", "years on list", "ceo", "eps",
-                     "employees", "revenues", "profits", "hq zip", "website", "market value", "global 500 rank", "revenue change", "profit change", "hq state"])
-
-    for company in companies:
-        writer.writerow([company.ranking, company.fullname, company.ticker, company.industry,
-                         company.sector, company.hqlocation, company.hqaddr, company.yearsonlist, company.ceo, company.eps,
-                         company.employees, company.revenues, company.profits, company.hqzip, company.website, company.mktval,
-                         company.global500rank, company.revchange, company.prftchange, company.hqstate])
-
-finally:
-    f.close()
