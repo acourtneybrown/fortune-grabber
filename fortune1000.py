bdf213c22a1a4234b9e2e4f2eb499dad4c040f3e
Adam Brown <acourtneybrown@github.com>
2018-12-17 21:59:09 -0700

    Actually fix for years 1996 to 2014

--- fortune1000.py
+++ fortune1000.py
@@ -2,9 +2,9 @@
 
 from bs4 import BeautifulSoup
 
-import csv
 import json
 import logging
+import os
 import re
 import sys
 import urllib.request
@@ -15,26 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 class Company:
-    ranking = 0
-    fullname = ""
-    ticker = ""
-    industry = ""
-    sector = ""
-    hqlocation = ""
-    hqaddr = ""
-    website = ""
-    yearsonlist = 0
-    ceo = ""
-    eps = 0
-    employees = 0
-    mktval=0
-    global500rank = 0
-    revenues = 0
-    revchange = 0.0
-    profits = 0
-    prftchange = 0.0
-    hqstate = ""
-    hqzip = ""
+    title = ""
+    rank = 0
+    name = ""
+    item = {}
 
 
 def usage():
@@ -49,21 +33,13 @@ def main():
     # Obtain companies
     year = sys.argv[1]
     companies = grab(year)
-    f = open(f"output/fortune1000-{year}.csv", "wt")
+    dirName = f"output/fortune1000/{year}"
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
 
-    try:
-        writer = csv.writer(f)
-        writer.writerow(["ranking", "full name", "ticker", "industry", "sector", "hq location", "hq address", "years on list", "ceo", "eps",
-                         "employees", "revenues", "profits", "hq zip", "website", "market value", "global 500 rank", "revenue change", "profit change", "hq state"])
-
-        for company in companies:
-            writer.writerow([company.ranking, company.fullname, company.ticker, company.industry,
-                             company.sector, company.hqlocation, company.hqaddr, company.yearsonlist, company.ceo, company.eps,
-                             company.employees, company.revenues, company.profits, company.hqzip, company.website, company.mktval,
-                             company.global500rank, company.revchange, company.prftchange, company.hqstate])
-
-    finally:
-        f.close()
+    for company in companies:
+        with open(f"{dirName}/{company.rank:04}-{company.name}", "wt") as outfile:
+            json.dump(company.item, outfile)
 
 
 def grab(year):
@@ -76,46 +52,26 @@ def grab(year):
 
     # Fetch for pages with data and process JSONs
     for i in range(0, 1000, 100):
-        page_url = "http://fortune.com/api/v2/list/2013055/expand/item/ranking/asc/{postid}/{index}/".format(postid=str(i), index=str(i+100))
+        page_url = "http://fortune.com/api/v2/list/{postid}/expand/item/ranking/asc/{start}/100/".format(postid=postid, start=str(i))
+        logger.info(page_url)
 
         try:
             page_data = json.load(urllib.request.urlopen(page_url), encoding='utf-8')
 
             for item in page_data["list-items"]:
                 company = Company()
-                try:
-                    company.ranking = item["meta"]["ranking"]
-                    company.fullname = item["meta"]["fullname"]
-                    company.ticker = item["meta"]["ticker"].upper()
-                    company.industry = item["meta"]["industry"]
-                    company.sector = item["meta"]["sector"]
-                    company.hqlocation = item["meta"]["hqlocation"]
-                    company.hqaddr = item["meta"]["hqaddr"]
-                    company.yearsonlist = item["meta"]["yearsonlist"]
-                    company.ceo = item["meta"]["ceo"]
-                    company.eps = item["meta"]["eps"]
-                    company.employees = item["meta"]["employees"]
-                    company.revenues = item["meta"]["revenues"]
-                    company.profits = item["meta"]["profits"]
-                    company.hqzip = item["meta"]["hqzip"]
-                    company.website = item["meta"]["website"]
-                    company.mktval = item["meta"]["mktval"]
-                    company.global500rank = item["meta"]["global500-rank"]
-                    company.revchange = item["meta"]["revchange"]
-                    company.prftchange = item["meta"]["prftchange"]
-                    company.hqstate = item["meta"]["hqstate"]
-                except KeyError:
-                    logger.error("Keyerror has occurered for " + str(company.fullname))
-
-                logger.info(str(company.ranking) + ". " + str(company.fullname) + " ; " + str(company.ceo))
+                company.title = item["title"]
+                company.rank = item["rank"]
+                company.name = item["name"]
+                company.item = item
+                logger.info(str(company.rank) + ". " + str(company.title))
 
                 companies.append(company)
-        except json.JSONDecodeError:
-            logger.error(str(i) + " to " + str(i+100) + " has JSONDecodeError.")
+        except json.JSONDecodeError as error:
+            logger.error(str(i) + " to " + str(i+100) + " has JSONDecodeError.", exc_info=error)
 
     return companies
 
 
 if __name__ == "__main__":
     main()
-
